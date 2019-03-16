b3568bf4ffee0197ff8d4d02ce0a506efd5aef09
Shahyn <shahynkamali@gmail.com>
2019-03-16 14:51:07 -0400

    fixed a piece

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,23 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Gallery = () => {
   return (
-<section className="wrapper">
-    <div className="gallery">
+    <section className="wrapper">
+      <div className="gallery">
         <div className="gallery-row">
-            <div className="gallery-img">
-                <img src={ require ('../assets/foto1.jpg')} alt="of shahyn kamali"/>
+          <div className="gallery-img">
+          <div class="content-overlay"></div>
+            <img src={require("../assets/foto1.jpg")} alt="of shahyn kamali" />
+            <div class="content-details fadeIn-bottom">
+              <h4 className="content-title">traveler</h4>
             </div>
-            <div className="gallery-img">
-                <img src={require('../assets/foto2.jpg')} alt="of shahyn kamali"/>
+          </div>
+          <div className="gallery-img">
+            <img src={require("../assets/foto2.jpg")} alt="of shahyn kamali" />
+            <div class="content-details fadeIn-bottom">
+              <h4 className="content-title">sommelier</h4>
             </div>
-            <div className="gallery-img">
-                <img src={require('../assets/foto3.jpg')} alt=" of shahyn kamali"/>
+          </div>
+          <div className="gallery-img">
+            <img src={require("../assets/foto3.jpg")} alt=" of shahyn kamali" />
+            <div class="content-details fadeIn-bottom">
+              <h4 className="content-title">developer</h4>
             </div>
+          </div>
         </div>
-    </div>
-</section>
-)
-}
+      </div>
+    </section>
+  );
+};
 
-export default Gallery
+export default Gallery;
