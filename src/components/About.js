13fcce7400d897092cb79414417c9fb2215a2ae2
Shahyn <shahynkamali@gmail.com>
2019-03-24 12:30:18 -0400

    fixcd

--- src/components/About.js
+++ src/components/About.js
@@ -7,31 +7,24 @@ import "animate.css/animate.min.css";
 const About = () => {
 return (
 <section className="wrapper">
-
     <div className="about-me">
-        <ScrollAnimation animateIn='fadeInLeft'
-        animateOut='flipOutY'>
+    <ScrollAnimation animateIn='fadeInLeft'animateOut='flipOutY'>
         <div className="header">
             <h2>about me</h2>
         </div>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn='fadeInRight'
-        animateOut='fadeOutLeft'>
+    </ScrollAnimation>
+    <ScrollAnimation animateIn='fadeInRight' animateOut='fadeOutLeft'>
         <div className="stories">
             <div className="about-me-image">
                 <img src={require("../assets/background.jpg")} alt="of shahyn kamali" />
             </div>
             <div className="my-philosophy">
-                <div className="my-philosophy-header">
-                    <h3>Thats me on an active volcano!</h3>
-                    <p>I am a front end developer, graduate of the HackerYou college of Technology with a passion for building interactive websites! 
-In a previous life I was crawling through wines cellars as a sommelier but I always had an interest in technology which allowed me to start coding. 
-The hospitality industry taught me how to create unique user experiences for guests and I apply the same mindset for when I build a website! 
-If I have a couple weeks to spare, you can find me traveling around the world finding pristine beaches and challenging hikes!</p>
-                </div>
+                <h3>Thats me on an active volcano!</h3>
+                <p>Hi, I’m Shahyn.
+                    I’m a HackerYou College of Technology graduate with a passion for building interactive websites!  In a previous life, I spent my days crawling through wine cellars as a Sommelier but my curiosity and interest in technology lead me to switch paths and pursue a career as a Front-End Developer instead. The hospitality industry taught me how to create unique user experiences for guests and I apply that same mindset to the websites I build.  If I have a couple weeks to spare, you can find me traveling to a new destination in search of pristine beaches and interesting food to try.</p>
             </div>
         </div>
-        </ScrollAnimation>
+    </ScrollAnimation>
     </div>
 </section>
 )
