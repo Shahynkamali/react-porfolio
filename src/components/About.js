411ec7e86aca2cfffa8917d02ee7780ab1853c67
Shahyn <shahynkamali@gmail.com>
2019-03-24 14:18:06 -0400

    finished

--- src/components/About.js
+++ src/components/About.js
@@ -19,7 +19,7 @@ return (
                 <img src={require("../assets/background.jpg")} alt="of shahyn kamali" />
             </div>
             <div className="my-philosophy">
-                <h3>Thats me on an active volcano!</h3>
+                <h3>That's me on an active volcano!</h3>
                 <p>Hi, I’m Shahyn.
                     I’m a HackerYou College of Technology graduate with a passion for building interactive websites!  In a previous life, I spent my days crawling through wine cellars as a Sommelier but my curiosity and interest in technology lead me to switch paths and pursue a career as a Front-End Developer instead. The hospitality industry taught me how to create unique user experiences for guests and I apply that same mindset to the websites I build.  If I have a couple weeks to spare, you can find me traveling to a new destination in search of pristine beaches and interesting food to try.</p>
             </div>
