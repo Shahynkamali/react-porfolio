b81198ceba056171a9366dc821b378639ba687fa
Shahyn <shahynkamali@gmail.com>
2019-03-31 13:36:54 -0400

    minor fix

--- src/components/About.js
+++ src/components/About.js
@@ -19,8 +19,9 @@ return (
                 <img src={require("../assets/me.jpeg")} alt="of shahyn kamali" />
             </div>
             <div className="my-philosophy">
-                <p>Hi, I’m Shahyn.
-                    I’m a HackerYou College of Technology graduate with a passion for building interactive websites!  In a previous life, I spent my days crawling through wine cellars as a Sommelier but my curiosity and interest in technology lead me to switch paths and pursue a career as a Front-End Developer instead. The hospitality industry taught me how to create unique user experiences for guests and I apply that same mindset to the websites I build.  If I have a couple weeks to spare, you can find me traveling to a new destination in search of pristine beaches and interesting food to try.</p>
+                <p>Hi, I’m Shahyn. I’m a HackerYou College of Technology graduate with a desire for building interactive websites!</p><br/>
+                <p> In a previous life, I spent my days crawling through wine cellars as a Sommelier but my curiosity and interest in technology lead me to switch paths and pursue a career as a Front-End Developer instead</p><br/>
+                <p> The hospitality industry taught me how to create unique user experiences for guests and I apply that same mindset to the websites I build.</p><br/><p>  If I have a couple weeks to spare, you can find me traveling to a new destination in search of pristine beaches and interesting food to try.</p>
             </div>
         </div>
     </ScrollAnimation>
