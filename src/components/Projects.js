639a2fca9e7d11dbf80f0627251cc05775e2233f
Shahyn <shahynkamali@gmail.com>
2019-04-08 15:02:38 +0200

    finito

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions} from 'react-mdl';
+import {Tabs, Tab, Grid, Cell} from 'react-mdl';
 import weather from '../assets/weather.png';
 import restaurant from '../assets/restaurant.png';
 import planted from '../assets/planted.png';
@@ -18,51 +18,72 @@ if(this.state.activeTab === 0){
     return( 
         <div className="cards">
             <div className="card">
-                <Card shadow={10}>
-                    <CardText>
-                        <CardTitle style={{color: "#fff", height: '176px', background: `url(${weather}), center / cover` }}>
-                            <h3>weather app</h3>
-                        </CardTitle>
-                        <div className="card-desc">
-                            <p>The application is a small weather app that fetches data from the Weather API and displays it on the UX.</p> 
+                <div className="flipcard">
+                    <div className="flipcard_front">
+                        <div className="flipcard_front_background">
+                            <div className="flipcard_front_image">
+                                <img src={weather} alt=""/>
+                            </div>
+                            <div className="flipcard_front_title">
+                                <h3>Weather App</h3>
+                            </div>
                         </div>
-                    </CardText>
-                    <CardActions border>
-                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Shahynkamali/Weather-App">GitHub</a>
-                    </CardActions>
-                </Card>
+                    </div>
+                    <div className="flipcard_back">
+                        <div className="flipcard_description">
+                        <p>The application is a small weather app that fetches data from the Weather API and displays it on the UX.</p>
+                        <div className="flipcard_actions">
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Shahynkamali/Weather-App"><i className="fab fa-github tooltip" ></i></a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="card">
-                <Card shadow={10}>
-                    <CardText>
-                        <CardTitle style={{color: "#fff", height: '176px', background: `url(${joke}), center / contain` }}>
-                            <h3>joke generator</h3>
-                        </CardTitle>
-                        <div className="card-desc">
-                            <p>Project that displays a daily joke, gets joke submissions and users can vote for the funniest joke. Data is stored in Firebase</p>
+                <div className="flipcard">
+                    <div className="flipcard_front">
+                        <div className="flipcard_front_background">
+                            <div className="flipcard_front_image">
+                                <img src={joke} alt=""/>
+                            </div>
+                            <div className="flipcard_front_title">
+                                <h3>Joke App</h3>
+                            </div>
                         </div>
-                    </CardText>
-                    <CardActions border>
-                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/shabora/parent-joke-project-6">GitHub</a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://parent-joke.firebaseapp.com/">Live Version</a>
-                    </CardActions>
-                </Card>
+                    </div>
+                    <div className="flipcard_back">
+                        <div className="flipcard_description">
+                         <p>Project that displays a daily joke, gets joke submissions and users can vote for the funniest joke. Data is stored in Firebase</p>
+                        <div className="flipcard_actions">
+                                <a target="_blank" rel="noopener noreferrer" href="https://github.com/shabora/parent-joke-project-6"><i className="fab fa-github tooltip" ></i></a>
+                                <a target="_blank" rel="noopener noreferrer" href="https://parent-joke.firebaseapp.com/"><i className="fab fa-internet-explorer"></i></a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="card">
-                <Card shadow={10}>
-                    <CardText>
-                        <CardTitle style={{color: "#fff", height: '176px', background: `url(${restaurant}) center / cover` }}>
-                            <h3>restaurant list</h3>
-                        </CardTitle>
-                        <div className="card-desc">
+                <div className="flipcard">
+                    <div className="flipcard_front">
+                        <div className="flipcard_front_background">
+                            <div className="flipcard_front_image">
+                                <img src={restaurant} alt=""/>
+                            </div>
+                            <div className="flipcard_front_title">
+                                <h3>Restaurant List App</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flipcard_back">
+                        <div className="flipcard_description">
                             <p>Application that finds various restaurants in North America and stores them in a top 10 list. Data is stored in Firebase</p>
+                        <div className="flipcard_actions">
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Shahynkamali/react_project_5"><i className="fab fa-github tooltip" ></i></a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://javascript-project-5.firebaseapp.com/"><i className="fab fa-internet-explorer"></i></a>
                         </div>
-                    </CardText>
-                    <CardActions border>
-                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Shahynkamali/react_project_5">GitHub</a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://javascript-project-5.firebaseapp.com/">Live Version</a>
-                    </CardActions>
-                </Card>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -71,21 +92,24 @@ if(this.state.activeTab === 0){
     return (
         <div className="cards">
         <div className="card">
-                <Card shadow={10}>
-                    <CardText>
-                        <CardTitle style={{color: "#fff", height: '176px', background: `url(${food}) center / cover` }}>
-                            <h3>recipe and wine finder</h3>
-                        </CardTitle>
-                        <div className="card-desc">
-                            <p>Application that fecthes recipes from an API and pairs them with a pre selected wine</p>
+                <div className="flipcard">
+                    <div className="flipcard_front">
+                    <div className="flipcard_front_title">
+                        <h3>Joke Supply App</h3>
+                    </div>
+                        <div className="flipcard_front_background">
+                        <img src={restaurant} alt=""/>
+                    </div>
+                    </div>
+                    <div className="flipcard_back">
+                        <div className="flipcard_description">
+                            <p>UC Berkeley CogSci Major with Concentration on Artificial Intelligence</p>
+                            <button title='Visit Website' onClick="window.open('https://jakehong0521.github.io')"><i className="fa fa-external-link"></i></button>
+                            <button title="Github Page" onClick="window.open('https://github.com/jakehong0521/jakehong0521.github.io')"><i className="fa fa-github"></i></button>
                         </div>
-                    </CardText>
-                    <CardActions border>
-                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/twobrownguys-moe-shahyn/project4">GitHub</a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://twobrownguys-moe-shahyn.github.io/project4/">Live Version</a>
-                    </CardActions>
-                </Card>
-            </div>
+                    </div>
+                </div>
+        </div>
     </div>
 
 
@@ -93,21 +117,28 @@ if(this.state.activeTab === 0){
 } else if (this.state.activeTab === 1){
     return (
         <div className="cards">
-        <div className="card">
-                <Card shadow={10}>
-                    <CardText>
-                        <CardTitle style={{color: "#fff", height: '176px', background: `url(${planted}) center / cover` }}>
-                            <h3>planted</h3>
-                        </CardTitle>
-                    <div className="card-desc">
-                        <p>PSD Designer project creating a multi page website using CSS Grid and Flexbox</p>
+            <div className="card">
+                <div className="flipcard">
+                    <div className="flipcard_front">
+                        <div className="flipcard_front_background">
+                            <div className="flipcard_front_image">
+                                <img src={planted} alt=""/>
+                            </div>
+                            <div className="flipcard_front_title">
+                                <h3>Weather App</h3>
+                            </div>
+                        </div>
                     </div>
-                    </CardText>
-                    <CardActions border>
-                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Shahynkamali/shahyn-kamali-project-two">GitHub</a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://shahynkamali.github.io/shahyn-kamali-project-two/">Live Version</a>
-                    </CardActions>
-                </Card>
+                    <div className="flipcard_back">
+                        <div className="flipcard_description">
+                            <p>PSD Designer project creating a multi page website using CSS Grid and Flexbox</p>
+                        <div className="flipcard_actions">
+                                <a target="_blank" rel="noopener noreferrer" href="https://github.com/Shahynkamali/shahyn-kamali-project-two"><i className="fab fa-github tooltip" ></i></a>
+                                <a target="_blank" rel="noopener noreferrer" href="https://shahynkamali.github.io/shahyn-kamali-project-two/"><i className="fab fa-internet-explorer"></i></a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             </div>
     </div>		
         )
